c5c7294399b9144fac8af24c3878949ce990bfee
Jisun Park <jisunpark.t@gmail.com>
2024-03-08 01:54:03 +0900

    Setting: Use react-router-dom for page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import AccountPage from './pages/AccountPage';
+import DashboardEditPage from './pages/DashboardEditPage';
+import DashboardPage from './pages/DashboardPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+
 export default function App() {
-  return <div>Taskify</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="account" element={<AccountPage />} />
+        <Route path="dashboard">
+          <Route index element={<DashboardPage />} />
+          <Route path="edit" element={<DashboardEditPage />} />
+        </Route>
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
