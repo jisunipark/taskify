bcbbb5377951670bdf66c4e776cea963f938fa74
Jisun Park <jisunpark.t@gmail.com>
2024-03-11 10:37:49 +0900

    Design: Construct common ui of button

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -1,20 +1,40 @@
-import classNames from 'classnames/bind';
-import styles from './Button.module.scss';
 import { ReactNode } from 'react';
+import classNames from 'classnames/bind';
+import addBoxIcon from '@/assets/add-box-white.svg';
+import styles from './Button.module.scss';
 
 const cx = classNames.bind(styles);
 
 interface ButtonProps {
   children: ReactNode;
+  type: 'normal' | 'auth' | 'modal' | 'invitation' | 'landing';
   color?: 'primary' | 'secondary' | 'disabled';
+  option?: 'short' | 'accept-reject';
 }
 
-export default function Button({ children, color = 'primary' }: ButtonProps) {
+// 해결해야 하는 것: 수락/거절
+export default function Button({ children, type, color = 'primary', option }: ButtonProps) {
   return (
     <button
-      className={cx('button', { primary: color === 'primary', secondary: color === 'secondary' })}
+      className={cx(
+        'button',
+        {
+          normal: type === 'normal',
+          auth: type === 'auth',
+          modal: type === 'modal',
+          invitation: type === 'invitation',
+          landing: type === 'landing',
+        },
+        {
+          primary: color === 'primary',
+          secondary: color === 'secondary',
+          disabled: color === 'disabled',
+        },
+        { short: option === 'short', 'accept-reject': option === 'accept-reject' }
+      )}
     >
-      {children}
+      {type === 'invitation' && <img src={addBoxIcon} alt="초대하기 아이콘" />}
+      <span>{children}</span>
     </button>
   );
 }
