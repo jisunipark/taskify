cc3c49d9ea542cd672a845a6639ef55776e6a782
Jisun Park <jisunpark.t@gmail.com>
2024-03-16 17:42:08 +0900

    Feat: Add error cases on auth pages

--- src/components/auth/auth-form/SignInForm.tsx
+++ src/components/auth/auth-form/SignInForm.tsx
@@ -1,21 +1,68 @@
+import { useForm } from 'react-hook-form';
 import classNames from 'classnames/bind';
 import AuthButton from '@/components/auth/auth-button/AuthButton';
 import styles from './AuthForm.module.scss';
 
 const cx = classNames.bind(styles);
 
+interface Inputs {
+  email: string;
+  password: string;
+}
+
 export default function SignInForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<Inputs>();
+
   return (
-    <form className={cx('form')}>
-      <div className={cx('form-section')}>
-        <label className={cx('label')}>이메일</label>
-        <input className={cx('input')} type="text" placeholder="이메일을 입력해주세요" />
+    <form
+      noValidate
+      onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
+      className={cx('form')}
+    >
+      <div className={cx('form-section', { error: errors.email })}>
+        <label htmlFor="email" className={cx('label')}>
+          이메일
+        </label>
+        <input
+          id="email"
+          type="email"
+          placeholder="이메일을 입력해주세요"
+          className={cx('input')}
+          {...register('email', {
+            required: '이메일을 입력해주세요',
+            pattern: {
+              value: /[a-zA-Z0-9+\-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.-]+$/,
+              message: '이메일 형식으로 작성해주세요',
+            },
+          })}
+          aria-invalid={errors.email ? false : true}
+        />
+        {errors.email && <small>{errors.email.message}</small>}
       </div>
-      <div className={cx('form-section')}>
-        <label className={cx('label')}>비밀번호</label>
-        <input className={cx('input')} type="password" placeholder="비밀번호를 입력해주세요" />
+      <div className={cx('form-section', { error: errors.password })}>
+        <label htmlFor="password" className={cx('label')}>
+          비밀번호
+        </label>
+        <input
+          id="password"
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          className={cx('input')}
+          {...register('password', {
+            required: '비밀번호를 입력해주세요',
+            minLength: {
+              value: 8,
+              message: '8자 이상 작성해주세요',
+            },
+          })}
+        />
+        {errors.password && <small>{errors.password.message}</small>}
       </div>
-      <AuthButton type="signin" />
+      <AuthButton type="signin" isSubmitting={isSubmitting} />
     </form>
   );
 }
