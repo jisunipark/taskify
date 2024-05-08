b6d14377b6c4ac06292b5c0e925baf0c0cae885e
Jisun Park <jisunpark.t@gmail.com>
2024-05-08 23:29:46 +0900

    Design: Construct user interface of board section

--- src/components/dashboard/task-item/TaskItem.tsx
+++ src/components/dashboard/task-item/TaskItem.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import Badge from '@/components/common/badge/Badge';
 import { ProfileItem } from '@/components/common/gnb/profile-stack/ProfileStack';
 import CalendarIcon from '@/assets/icons/CalendarIcon';
 import MockImg from '../../../assets/images/img.png';
@@ -12,7 +13,7 @@ export default function TaskItem() {
       <img src={MockImg} alt="이미지" className={cx('img')} />
       <div className={cx('info')}>
         <span className={cx('title')}>새로운 일정 관리 Taskify</span>
-        <span className={cx('badge')}>일반</span>
+        <Badge>일반</Badge>
         <div className={cx('detail')}>
           <div className={cx('date')}>
             <CalendarIcon />
