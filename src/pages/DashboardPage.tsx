02515e74ac7c0b141674b9f165fe57e01e5bb30f
Jisun Park <jisunpark.t@gmail.com>
2024-05-08 22:20:57 +0900

    Design: Control margin of pages

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,13 @@
 import Gnb from '@/components/common/gnb/Gnb';
 import Lnb from '@/components/common/lnb/Lnb';
+import BoardSection from '@/components/dashboard/board-section/BoardSection';
 
 export default function DashboardPage() {
   return (
-    <div>
+    <div className="board page">
       <Lnb />
       <Gnb />
+      <BoardSection />
     </div>
   );
 }
