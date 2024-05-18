1c5e9adf54cc5aeeba5bcc4330d941fd77ee384b
Jisun Park <jisunpark.t@gmail.com>
2024-05-18 15:43:12 +0900

    Refactor: Make modals belongs to id='modal' element by using portal

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
 import classNames from 'classnames/bind';
 import styles from './Modal.module.scss';
 
@@ -9,5 +10,8 @@ interface ModalProps {
 }
 
 export default function Modal({ children }: ModalProps) {
-  return <div className={cx('wrap')}>{children}</div>;
+  const modalEl = document.getElementById('modal');
+  if (!modalEl) return;
+
+  return createPortal(<div className={cx('wrap')}>{children}</div>, modalEl);
 }
