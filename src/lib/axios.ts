cf9c8e74fc6ae9e00479189ee9c37e4c2240e6c7
Jisun Park <jisunpark.t@gmail.com>
2024-05-20 01:09:07 +0900

    Feat: Add common authorization header to axiox instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,8 @@ const instance = axios.create({
   baseURL: 'https://sp-taskify-api.vercel.app/taskify/',
 });
 
+const accessToken = localStorage.getItem('accessToken');
+
+instance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
 export default instance;
