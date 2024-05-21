d79db1c06e0537a7419c55452459ae351e408017
Jisun Park <jisunpark.t@gmail.com>
2024-05-21 21:10:29 +0900

    Feat: Add Modals to auth pages

--- src/components/common/modal/confirm-modal/ConfirmModal.tsx
+++ src/components/common/modal/confirm-modal/ConfirmModal.tsx
@@ -1,29 +1,31 @@
 import classNames from 'classnames/bind';
 import ButtonPair from '@/components/common/button-pair/ButtonPair';
 import Button from '@/components/common/button/Button';
-import ModalWrapper from '@/components/common/modal/ModalWrapper';
 import styles from './ConfirmModal.module.scss';
 
 const cx = classNames.bind(styles);
 
 interface ConfirmModalProps {
+  closeClick: () => void;
   message: string;
   hasButtonPair?: boolean;
 }
 
-export default function ConfirmModal({ message, hasButtonPair = false }: ConfirmModalProps) {
+export default function ConfirmModal({
+  closeClick,
+  message,
+  hasButtonPair = false,
+}: ConfirmModalProps) {
   return (
-    <ModalWrapper>
-      <form method="dialog" className={cx('wrap')}>
-        <span className={cx('modal-text')}>{message}</span>
-        <div className={cx('button')}>
-          {hasButtonPair ? (
-            <ButtonPair type="modal" primaryText="삭제" secondaryText="취소" />
-          ) : (
-            <Button type="modal">확인</Button>
-          )}
-        </div>
-      </form>
-    </ModalWrapper>
+    <form method="dialog" className={cx('wrap')}>
+      <span className={cx('modal-text')}>{message}</span>
+      <div onClick={closeClick} className={cx('button')}>
+        {hasButtonPair ? (
+          <ButtonPair type="modal" primaryText="삭제" secondaryText="취소" />
+        ) : (
+          <Button type="modal">확인</Button>
+        )}
+      </div>
+    </form>
   );
 }
